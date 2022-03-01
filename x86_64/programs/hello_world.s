13a67076126848e034199b59c516c6f0eeb43ed5
Naveenprakash <51453952+ssnaveenprakash@users.noreply.github.com>
2022-03-01 19:35:38 +0000

    adds print method

--- x86_64/programs/hello_world.s
+++ x86_64/programs/hello_world.s
@@ -1,16 +1,20 @@
 .global _start
 .global exit
+.global print 
 .data
 .hello_world:
         .ascii "Hello\0"
 
 .text
 _start:
-        movq $6,%rdx 
-        movq $.hello_world,%rsi
-        movq $1 , %rdi
-        movq $1 , %rax
-	syscall
+	pushq %rbp
+	movq %rsp,%rbp
+        movq $rbx,%rbx 
+        movq $.hello_world,%r12
+	call print
+ #        movq $1 , %rdi
+ #        movq $1 , %rax
+	# syscall
 	call exit
 
 
---
+++ x86_64/programs/utils/print.s
@@ -0,0 +1,20 @@
+.global print
+print:
+	pushq %rbp
+	pushq %rbx
+	pushq %r12
+	movq %rsp,%rbp
+	
+	
+	movq %rbx , %rdx
+	movq %r12 , %rsi
+        movq $1 , %rdi
+        movq $1 , %rax
+	syscall
+
+	movq %rbp,%rsp
+	popq %r12
+	popq %rbx
+	popq %rbp
+	ret
+
