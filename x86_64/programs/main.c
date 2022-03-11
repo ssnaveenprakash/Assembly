8b1d37dad478dd522dc2946877924546d9ae697a
Naveenprakash <51453952+ssnaveenprakash@users.noreply.github.com>
2022-03-11 04:12:49 +0000

    implements changes

--- x86_64/programs/main.c
+++ x86_64/programs/main.c
@@ -2,7 +2,7 @@
 #include"assembly_functions.h"
 
 int main() {
-	long sum = add(3,2,3,4,1); 
+	long sum = add(4,1,2,3,4,5,6,7); 
 	printf("%ld \n",sum);
 
 	sum = add(3,1,2,1,1); 
@@ -10,3 +10,4 @@ int main() {
 	
 	return 0;
 }
+//  x/24bd $rbp+16
